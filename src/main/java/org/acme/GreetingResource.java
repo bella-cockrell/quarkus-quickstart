a12d58f894860deceafb4e05bdaf569278d8eccb
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-06-16 20:46:36 +0200

    Add endpoint with query param

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,12 +1,25 @@
 package org.acme;
 
+import io.quarkus.runtime.Quarkus;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.services.GreetingService;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+        return service.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
